4df5d68afe0d7de76ed09028620b303fd5a0d89f
Sylviaani <sylviaani26@gmail.com>
2023-07-26 09:59:01 -0700

    commit message

--- shell.c
+++ shell.c
@@ -18,4 +18,40 @@ void handle_arguments(char *args[], char *err_msg)
 	pid_t pid;
 	int status;
 
-	if (_strcmp(args[0], "ls"ss
+	if (_strcmp(args[0], "ls") == 0)
+	{
+		args[0] = "/bin/ls";
+	}
+	if (access(args[0], F_OK) == -1)
+	{
+		perror(err_msg);
+		return;
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		perror(err_msg);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			perror(err_msg);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror(err_msg);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+/**
+ * execute_command - function that handles command lines
+ * @args: argument one
+ * @err_msg: argument two
+ *
